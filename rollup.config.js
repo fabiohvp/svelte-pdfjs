4b03179dbb796b7944176ce255dd44468981de2b
Fábio Henriques <fabiohvp@hotmail.com>
2020-01-18 02:05:03 -0200

    initial

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,31 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import pkg from './package.json';
+import builtins from "rollup-plugin-node-builtins";
+import commonjs from "@rollup/plugin-commonjs";
+import pkg from "./package.json";
+import postcss from "rollup-plugin-postcss";
+import resolve from "@rollup/plugin-node-resolve";
+import svelte from "rollup-plugin-svelte";
 
 const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
+  .replace(/^\w/, m => m.toUpperCase())
+  .replace(/-\w/g, m => m[1].toUpperCase());
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.module, 'format': 'es' },
-		{ file: pkg.main, 'format': 'umd', name }
-	],
-	plugins: [
-		svelte(),
-		resolve()
-	]
+  input: "src/index.js",
+  output: [
+    { file: pkg.module, format: "es" },
+    { file: pkg.main, format: "umd", name }
+  ],
+  plugins: [
+    postcss(),
+    svelte(),
+    resolve({
+      browser: true,
+      dedupe: importee =>
+        importee === "svelte" || importee.startsWith("svelte/"),
+      preferBuiltins: true
+    }),
+    builtins(),
+    commonjs()
+  ]
 };
--- src/Component.svelte
+++ src/Component.svelte
@@ -1,1 +1,82 @@
-<!-- your code here -->
+<script context="module">
+  let workerSrc = "/pdfjs/pdf.worker.min.js";
+
+  export function setWorkerSrc(src) {
+    workerSrc = src;
+  }
+</script>
+
+<script>
+  import { createEventDispatcher, onDestroy, onMount } from "svelte";
+  import pdfjs from "pdfjs-dist";
+  import pdfjsViewer from "pdfjs-dist/web/pdf_viewer.js";
+  import "pdfjs-dist/web/pdf_viewer.css";
+
+  pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
+
+  const dispatch = createEventDispatcher();
+
+  export let src;
+  export let scale = "page-width";
+  export let cMapSrc = "/pdfjs/cmaps";
+  export let cMapPacked = true;
+
+  export let pdfViewerOptions = {};
+  export let documentOptions = {
+    cMapUrl: cMapSrc,
+    cMapPacked
+  };
+
+  let containerRef;
+  let pdfViewer;
+
+  function load(src) {
+    // (Optionally) enable hyperlinks within PDF files.
+    const pdfLinkService = new pdfjsViewer.PDFLinkService();
+
+    pdfViewer = new pdfjsViewer.PDFViewer({
+      container: containerRef,
+      linkService: pdfLinkService,
+      ...pdfViewerOptions
+    });
+
+    pdfLinkService.setViewer(pdfViewer);
+
+    // Loading document.
+    const loadingTask = pdfjs.getDocument({
+      src,
+      ...documentOptions
+    });
+
+    loadingTask.promise.then(function(pdfDocument) {
+      // Document loaded, specifying document for the viewer and the (optional) linkService.
+      pdfViewer.setDocument(pdfDocument);
+      pdfLinkService.setDocument(pdfDocument, null);
+    });
+  }
+
+  $: if (containerRef && src) {
+    load(src);
+  }
+
+  function pagesInit() {
+    if (pdfViewer) {
+      // We can use pdfViewer now, e.g. let's change default scale.
+      pdfViewer.currentScaleValue = scale;
+      dispatch("load", pdfViewer);
+    }
+  }
+
+  onMount(async () => {
+    //load(src);
+    document.addEventListener("pagesinit", pagesInit);
+  });
+
+  onDestroy(async () => {
+    document.removeEventListener("pagesinit", pagesInit);
+  });
+</script>
+
+<div class="w-full h-full" bind:this={containerRef}>
+  <div class="pdfViewer" />
+</div>
