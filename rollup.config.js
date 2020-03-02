d2119bdff50975ce72b9ab0909694456c240f6e1
Fábio Henriques Viana Pinto <fabio.henriques@xdevel.com.br>
2020-03-01 22:23:41 -0300

    fixed rollup and increase patch version

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,25 @@
 import builtins from "rollup-plugin-node-builtins";
 import commonjs from "@rollup/plugin-commonjs";
-import pkg from "./package.json";
 import postcss from "rollup-plugin-postcss";
 import resolve from "@rollup/plugin-node-resolve";
 import svelte from "rollup-plugin-svelte";
 
-const name = pkg.name
-  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
-  .replace(/^\w/, m => m.toUpperCase())
-  .replace(/-\w/g, m => m[1].toUpperCase());
-
 export default {
-  input: "src/index.js",
-  output: [
-    { file: pkg.module, format: "es" },
-    { file: pkg.main, format: "umd", name }
-  ],
-  plugins: [
-    postcss(),
-    svelte(),
-    resolve({
-      browser: true,
-      dedupe: importee =>
-        importee === "svelte" || importee.startsWith("svelte/"),
-      preferBuiltins: true
-    }),
-    builtins(),
-    commonjs()
-  ]
+	input: "src/index.js",
+	output: {
+		dir: "dist",
+		format: "es"
+	},
+	plugins: [
+		postcss(),
+		svelte(),
+		resolve({
+			browser: true,
+			dedupe: importee =>
+				importee === "svelte" || importee.startsWith("svelte/"),
+			preferBuiltins: true
+		}),
+		builtins(),
+		commonjs()
+	]
 };
